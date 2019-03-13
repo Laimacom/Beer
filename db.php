162c0574fcd3d660883fa1d2b0f38f210086f0da
Laimacom <laima.grivaciauskaite@gmail.com>
2019-03-13 16:27:32 +0200

    Distance between two points(LAT/LONG)

--- db.php
+++ db.php
@@ -14,7 +14,7 @@ function connectDB()
     return $conn;
 }
 
-function selectDB($conn){
+function selectFromBreweries($conn){
     $row = [];
     $sql = "SELECT * FROM breweries WHERE id = 1";
     $result = $conn->query($sql);
@@ -22,7 +22,37 @@ function selectDB($conn){
     var_dump($row);
     return $row;
 }
+function selectLatLon($conn){
+    $row = [];
+    $sql = "SELECT * FROM geocodes WHERE id = 3";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    var_dump($row);
+    return $row;
+}
 
 $conn = connectDB();
-selectDB($conn);
+selectFromBreweries($conn);
+echo '<br>';
+$row = selectLatLon($conn);
+$lat2 = $row['latitude'];
+$lon2 = $row['longitude'];
+
+$lat1 = 51.355468;
+$lon1 = 11.100790;
+
+function getDistanceBetweenPointsNew($lat1, $lon1, $lat2, $lon2, $unit = 'Km') {
+    $theta = $lon1 - $lon2;
+    $distance = (sin(deg2rad($lat1)) * sin(deg2rad($lat2))) + (cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta)));
+    $distance = acos($distance);
+    $distance = rad2deg($distance);
+    $distance = $distance * 60 * 1.1515;
+    switch($unit) {
+        case 'Mi': break;
+        case 'Km' : $distance = $distance * 1.609344;
+    }
+    return (round($distance,2));
+}
+echo '<br>';
+echo getDistanceBetweenPointsNew($lat1, $lon1, $lat2, $lon2, $unit = 'Km');
 
