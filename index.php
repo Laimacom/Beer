833ec81a53debf4bb0380297fce1b35cd8c261c8
Laimacom <laima.grivaciauskaite@gmail.com>
2019-03-17 16:53:47 +0200

    Form for writing different home LAT/LONG

--- index.php
+++ index.php
@@ -2,64 +2,69 @@
 namespace Beer;
 require_once 'models.php';
 require_once 'Distance.php';
+require_once 'views.php';
 
-$conn = connectDB();
-$items = selectLatLon($conn);
+form();
+if (isset($_POST['lat1'])) {
+    $conn = connectDB();
+    $items = selectLatLon($conn);
 
-$lat1 = 51.355468;
-$lon1 = 11.100790;
-echo 'Home is ' . $lat1 . " " . $lon1;
-echo '<br>';
+    $lat1 = $_POST['lat1'];
+    $lon1 = $_POST['lon1'];
+    echo 'Home is ' . $lat1 . " " . $lon1;
+    echo '<br>';
 
 //var_dump(Distance::getClosest($lat1, $lon1, $items, $decimals = 1, $unit = 'km'));
-$firstBrewery = Distance::getClosest($lat1, $lon1, $items, $decimals = 1, $unit = 'km');
-$id1 = $firstBrewery['brewery_id'];
-$distance1 = $firstBrewery['distance'];
-$lat1st = $firstBrewery['latitude'];
-$lon1st = $firstBrewery['longitude'];
+    $firstBrewery = Distance::getClosest($lat1, $lon1, $items, $decimals = 1, $unit = 'km');
+    $id1 = $firstBrewery['brewery_id'];
+    $distance1 = $firstBrewery['distance'];
+    $lat1st = $firstBrewery['latitude'];
+    $lon1st = $firstBrewery['longitude'];
 
-$breweries = selectFromBreweries($conn);
+    $breweries = selectFromBreweries($conn);
 
-foreach ($breweries as $key => $brewery) {
-    $name = $brewery ['name'];
-    if ($id1 == $brewery['id']){
-        echo 'First Brewery is ' . $name . '. Distance ' . $distance1 . 'km';
+    foreach ($breweries as $key => $brewery) {
+        $name = $brewery ['name'];
+        if ($id1 == $brewery['id']) {
+            echo 'First Brewery is ' . $name . '. Distance ' . $distance1 . 'km';
+        }
     }
-}
 
-$secondBrewery = Distance::getClosest($lat1st, $lon1st, $items, $decimals = 1, $unit = 'km');
-echo '<br>';
+    $secondBrewery = Distance::getClosest($lat1st, $lon1st, $items, $decimals = 1, $unit = 'km');
+    echo '<br>';
 //var_dump($secondBrewery);
-$id2 = $secondBrewery['brewery_id'];
-$distance2 = $secondBrewery['distance'];
-$lat2st = $secondBrewery['latitude'];
-$lon2st = $secondBrewery['longitude'];
+    $id2 = $secondBrewery['brewery_id'];
+    $distance2 = $secondBrewery['distance'];
+    $lat2st = $secondBrewery['latitude'];
+    $lon2st = $secondBrewery['longitude'];
 
-foreach ($breweries as $key => $brewery) {
-    $name = $brewery ['name'];
-    if ($id2 == $brewery['id']){
-        echo 'Second Brewery is ' . $name . '. Distance ' . $distance2 . 'km';
+    foreach ($breweries as $key => $brewery) {
+        $name = $brewery ['name'];
+        if ($id2 == $brewery['id']) {
+            echo 'Second Brewery is ' . $name . '. Distance ' . $distance2 . 'km';
+        }
     }
-}
 
-$thirdBrewery = Distance::getClosest($lat2st, $lon2st, $items, $decimals = 1, $unit = 'km');
-echo '<br>';
+    $thirdBrewery = Distance::getClosest($lat2st, $lon2st, $items, $decimals = 1, $unit = 'km');
+    echo '<br>';
 //var_dump($thirdBrewery);
-$id3 = $thirdBrewery['brewery_id'];
-$distance3 = $thirdBrewery['distance'];
-$lat3st = $thirdBrewery['latitude'];
-$lon3st = $thirdBrewery['longitude'];
+    $id3 = $thirdBrewery['brewery_id'];
+    $distance3 = $thirdBrewery['distance'];
+    $lat3st = $thirdBrewery['latitude'];
+    $lon3st = $thirdBrewery['longitude'];
 
-foreach ($breweries as $key => $brewery) {
-    $name = $brewery ['name'];
-    if ($id3 == $brewery['id']){
-        echo 'Third Brewery is ' . $name . '. Distance ' . $distance3 . 'km';
+    foreach ($breweries as $key => $brewery) {
+        $name = $brewery ['name'];
+        if ($id3 == $brewery['id']) {
+            echo 'Third Brewery is ' . $name . '. Distance ' . $distance3 . 'km';
+        }
     }
-}
 
-$totalDistance = $distance1 + $distance2 + $distance3;
-echo '<br>';
-echo 'Total distance: ' . $totalDistance;
+    $goingHome = Distance::between($lat1, $lon1, $lat3st, $lon3st, $decimals = 1, $unit = 'km');
+    $totalDistance = $distance1 + $distance2 + $distance3 + $goingHome;
+    echo '<br>';
+    echo 'Total distance: ' . $totalDistance . ' km';
+}
 
 
 
